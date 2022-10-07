be1f9237dc234205d5b1a66919744189f6f769fc
Aishwarya Iyer <aradhakrishnaniye@deloitte.com>
2022-10-07 12:14:27 +0530

    Initial Commit

---
+++ SplashScreen/SplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreenApp.swift
+//  SplashScreen
+//
+//  Created by Radhakrishnan Iyer, Aishwarya on 07/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
